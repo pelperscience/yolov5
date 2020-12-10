bea961d51e158b18974e3848a64951887059dfb8
pelperscience <66783894+pelperscience@users.noreply.github.com>
2020-12-10 15:05:37 +0800

    Update train.py

--- train.py
+++ train.py
@@ -420,16 +420,16 @@ if __name__ == '__main__':
     parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training') ########################################### const=False
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint') ########################################### action='store_False'
-    parser.add_argument('--notest', action='store_true', help='only test final epoch') ########################################### action='store_False'
-    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check') ########################################### action='store_False'
-    parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters') ########################################### action='store_False'
+    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
+    parser.add_argument('--notest', action='store_true', help='only test final epoch')
+    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
+    parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
     parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
     parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
     parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
-    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset') ########################################### action='store_False'
+    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
     parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
     parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
     parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
