15b9274b955c76c04b6b78fb03d68490d2d2ab69
pelperscience <66783894+pelperscience@users.noreply.github.com>
2020-12-12 13:00:28 +0800

    Update train.py

--- train.py
+++ train.py
@@ -416,23 +416,23 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path') ########################################### 修改coco128.yaml
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs') #
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training') ########################################### const=False
+    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs') ###
+    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes') ### img-size must be multiple of max stride 32
+    parser.add_argument('--rect', action='store_true', help='rectangular training') # 是否采用矩形训练，默认False
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
     parser.add_argument('--notest', action='store_true', help='only test final epoch')
     parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
     parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
-    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training') #
+    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket') # 谷歌云盘bucket，一般不会用到
+    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training') ### 是否提前缓存图片到内存，以加快训练速度，默认False
     parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%') #
-    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
-    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer') #
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
+    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%') ###
+    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset') # 数据集是否只有一个类别，默认False
+    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer') ###
+    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode') # 是否使用跨卡同步BN,在DDP模式使用
+    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify') # gpu编号
     parser.add_argument('--log-imgs', type=int, default=16, help='number of images for W&B logging, max 100')
     parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
     parser.add_argument('--project', default='runs/train', help='save to project/name') ########################################### default='drive/MyDrive/colab/CV_jiejing/yolov5/runs/train'
@@ -440,22 +440,23 @@ if __name__ == '__main__':
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     opt = parser.parse_args()
 
-    # Set DDP variables
+    # Set DDP variables 设置DDP模式的参数
     opt.total_batch_size = opt.batch_size
-    opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
-    opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
+    opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1 # world_size:表示全局进程个数
+    opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1 # global_rank:进程编号
     set_logging(opt.global_rank)
     if opt.global_rank in [-1, 0]:
-        check_git_status()
+        check_git_status() # 检查你的代码版本是否为最新的(不适用于windows系统)
 
     # Resume
     if opt.resume:  # resume an interrupted run
-        ckpt = opt.resume if isinstance(opt.resume, str) else get_latest_run()  # specified or most recent path
+        ckpt = opt.resume if isinstance(opt.resume, str) else get_latest_run()  # specified or most recent path 如果resume是str,则表示传入的是模型的路径地址
+                                                                                                              # get_latest_run()函数获取runs文件夹中最近的last.pt
         assert os.path.isfile(ckpt), 'ERROR: --resume checkpoint does not exist'
-        with open(Path(ckpt).parent.parent / 'opt.yaml') as f:
+        with open(Path(ckpt).parent.parent / 'opt.yaml') as f: # opt参数也全部替换
             opt = argparse.Namespace(**yaml.load(f, Loader=yaml.FullLoader))  # replace
         opt.cfg, opt.weights, opt.resume = '', ckpt, True
-        logger.info('Resuming training from %s' % ckpt)
+        logger.info('Resuming training from %s' % ckpt) # ckpt='./drive/MyDrive/colab/CV_jiejing/yolov5/runs/train/exp2/weights/last.pt'
     else:
         # opt.hyp = opt.hyp or ('hyp.finetune.yaml' if opt.weights else 'hyp.scratch.yaml')
         opt.data, opt.cfg, opt.hyp = check_file(opt.data), check_file(opt.cfg), check_file(opt.hyp)  # check files
