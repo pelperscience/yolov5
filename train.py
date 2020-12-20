aa4dfc8780c08bb58905bbb17acbdf135fe35881
pelperscience <66783894+pelperscience@users.noreply.github.com>
2020-12-20 15:58:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -531,7 +531,7 @@ if __name__ == '__main__':
         if opt.bucket:
             os.system('gsutil cp gs://%s/evolve.txt .' % opt.bucket)  # download evolve.txt if exists
 
-        for _ in range(300):  # generations to evolve
+        for _ in range(300):  # generations to evolve ########################################### evolve次数
             if Path('evolve.txt').exists():  # if evolve.txt exists: select best hyps and mutate ########################################### 'drive/MyDrive/colab/CV_jiejing/yolov5/runs/evolve.txt'
                 # Select parent(s)
                 parent = 'single'  # parent selection method: 'single' or 'weighted'
